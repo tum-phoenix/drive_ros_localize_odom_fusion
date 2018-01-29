7996b52ddec8d209f45752172a131fd551bec6d4
Unknown <dev-faha@t-online.de>
2018-01-29 09:49:14 +0100

    update scripts for kalman tuning
    - use new method for loss calculation
    - added .launch file parameter in trial runner
    - use loguniform instead of uniform for search space
    - remove results processing at the end (will be done via jupyter notebook)

--- scripts/kalmanTuning.py
+++ scripts/kalmanTuning.py
@@ -23,13 +23,26 @@ def objective(params):
     from hyperopt import STATUS_OK, STATUS_FAIL
     from collections import deque
 
+
+    # path definitions
     path_home = "/home/fabian"
     path_catkin_ws = path_home + "/catkin_ws"
     path_ros_package = path_catkin_ws + "/src/drive_ros_config/modules/drive_ros_imu_odo_odometry"
-    path_config_file = path_ros_package + "/config/phoenix_cc2017.yaml"
+    path_config_file = path_ros_package + "/config/ftm_rc_car_1.yaml"
     path_trial_runner = path_ros_package + "/scripts/runTrial.sh"
-    path_bag_file = path_ros_package + "/data/circle.bag"
+    path_bag_file = path_ros_package + "/data/circle_002.bag"
+    path_launch_file = path_ros_package + "/data/circle_002.launch"
     path_results = "/data/KalmanTuningLogs/"
+
+    # TODO: make them bag specific (like: 1m error of 100m length)
+    # normalize results by this values
+    x_norm = 0.5 # meters
+    y_norm = 0.5 # meters
+    theta_norm = 0.17 # radiant
+
+    x_var_norm = 1.0 # meters
+    y_var_norm = 1.0 # meters
+    theta_var_norm = 0.34 # radiant
 
     # get trial number
     trial = random.getrandbits(128)
@@ -63,11 +76,14 @@ def objective(params):
     yaml.dump(config, config_out)
 
     # start trial runner
-    p = subprocess.run([path_trial_runner, "--trail", str(trial),
+    p = subprocess.run([path_trial_runner,
+                        "--trail", str(trial),
                         "--logdir", path_results + str(trial),
                         "--bag", path_bag_file,
                         "--config", yaml_config,
-                        "--catkin_ws", path_catkin_ws], shell=False)
+                        "--catkin_ws", path_catkin_ws,
+                        "--launch", path_launch_file ],
+                        shell=False)
 
     # evaluate return code of trial runner
     if 0 == p.returncode:
@@ -86,10 +102,21 @@ def objective(params):
         except IndexError:  # empty file
             lastrow = None
 
-        x = float(lastrow[2])  # x position at the end
-        y = float(lastrow[3])  # y position at the end
+        x = float(lastrow[1])            # x position at the end
+        y = float(lastrow[2])            # y position at the end
+        theta = float(lastrow[6])        # theta at the end
 
-        loss = math.sqrt(x * x + y * y)  # distance from start to the end (assume we drive a circle)
+        x_var = float(lastrow[7])        # x position at the end
+        y_var = float(lastrow[14])       # y position at the end
+        theta_var = float(lastrow[42])   # theta at the end
+
+
+        loss = math.sqrt( x/x_norm * x/x_norm
+                        + y/y_norm * y/y_norm
+                        + theta/theta_norm * theta/theta_norm
+                        + x_var/x_var_norm * x_var/x_var_norm
+                        + y_var/y_var_norm * y_var/y_var_norm
+                        + theta_var/theta_var_norm * theta_var/theta_var_norm )
         print("  Loss is: " + str(loss))
 
     # return trial results
@@ -97,7 +124,13 @@ def objective(params):
         'loss': loss,
         'status': status,
         'eval_time': time.time(),
-        'log_path': path_results + str(trial)
+        'log_path': path_results + str(trial),
+        'x_end': x,
+        'y_end': y,
+        'theta_end': theta,
+        'x_var_end': x_var,
+        'y_var_end': y_var,
+        'theta_var_end': theta_var,
     }
 
 
@@ -118,12 +151,12 @@ if __name__ == "__main__":
     # create search space
     print("Create search space")
     space = {
-        'x': hp.uniform('x', 0, 1),
-        'y': hp.uniform('y', 0, 1),
-        'a': hp.uniform('a', 0, 1),
-        'v': hp.uniform('v', 0, 1),
-        'theta': hp.uniform('theta', 0, 1),
-        'omega': hp.uniform('omega', 0, 1)
+        'x': hp.loguniform('x', 0, 1),
+        'y': hp.loguniform('y', 0, 1),
+        'a': hp.loguniform('a', 0, 1),
+        'v': hp.loguniform('v', 0, 1),
+        'theta': hp.loguniform('theta', 0, 1),
+        'omega': hp.loguniform('omega', 0, 1)
     }
 
     # some settings
@@ -141,14 +174,3 @@ if __name__ == "__main__":
     print("Start fmin ...")
     best = fmin(objective, space, algo, max_evals, trials)
     print("Finished fmin :=)")
-
-    # save results to file
-    with open(path_results + args.experiment + "_results.csv", 'w') as csvfile:
-        fieldnames = ['eval_time', 'status', 'loss', 'log_path']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for result in trials.results:
-            print("Write result of " + str(result['eval_time']) + " to file ...")
-            writer.writerow(result)
-
-
--- scripts/runTrial.sh
+++ scripts/runTrial.sh
@@ -3,106 +3,124 @@
 # print help and exit
 function help {
  echo "Use the following arguments:"
- echo "  --trail <nr>"
+ echo "  --trial <nr>"
  echo "  --logdir <dir>"
  echo "  --bag <bagfile>"
  echo "  --config <vehicle_config>"
+ echo "  --launch <launch_file>"
  echo "  --catkin_ws <catkin_ws_dir>"
  echo ""
  exit 1
 }
 
 # parse arguments
-TRAIL=-1
+TRIAL=-1
 LOGDIR="-1"
 BAGFILE="-1"
 VEHICLECONFIG="-1"
 CATKIN_WS="-1"
+LAUNCH="-1"
 
 while [[ $# -gt 0 ]]
 do
-	key="$1"
-	case $key in
-		--trail)
-		TRAIL="$2"
-		shift # past argument
-		shift # past value
-		;;
-		--logdir)
-		LOGDIR="$2"
-		shift # past argument
-		shift # past value
-		;;
-		--bag)
-		BAGFILE="$2"
-		shift # past argument
-		shift # past value
-		;;
-		--config)
-		VEHICLECONFIG="$2"
-		shift # past argument
-		shift # past value
-		;;
-		--catkin_ws)
-		CATKIN_WS="$2"
-		shift # past argument
-		shift # past value
-		;;
-		*)    # unknown option
-		help
-		;;
-	esac
+        key="$1"
+        case $key in
+                --trial)
+                TRIAL="$2"
+                shift # past argument
+                shift # past value
+                ;;
+                --logdir)
+                LOGDIR="$2"
+                shift # past argument
+                shift # past value
+                ;;
+                --bag)
+                BAGFILE="$2"
+                shift # past argument
+                shift # past value
+                ;;
+                --config)
+                VEHICLECONFIG="$2"
+                shift # past argument
+                shift # past value
+                ;;
+                --launch)
+                LAUNCH="$2"
+                shift # past argument
+                shift # past value
+                ;;
+                --catkin_ws)
+                CATKIN_WS="$2"
+                shift # past argument
+                shift # past value
+                ;;
+                *)    # unknown option
+                help
+                ;;
+        esac
 done
 
 # check if all parameters are set
-if [ $TRAIL = "-1" ]
+if [ $TRIAL = "-1" ]
 then
-	help
+        help
 fi
 
 if [ "$LOGDIR" = "-1" ]
 then
-	help
+        help
 fi
 
 if [ "$BAGFILE" = "-1" ]
 then
-	help
+        help
 fi
 
 if [ "$VEHICLECONFIG" = "-1" ]
 then
-	help
+        help
+fi
+
+if [ "$LAUNCH" = "-1" ]
+then
+        help
 fi
 
 if [ "$CATKIN_WS" = "-1" ]
 then
-	help
+        help
 fi
 
 # check if paths are valid
 if [ ! -d "$LOGDIR" ]
 then
   echo "$LOGDIR does not exists!"
-	exit 2
+        exit 2
 fi
 
 if [ ! -f "$BAGFILE" ]
 then
   echo "$BAGFILE does not exists!"
-	exit 3
+        exit 3
 fi
 
 if [ ! -f "$VEHICLECONFIG" ]
 then
   echo "$VEHICLECONFIG does not exists!"
-	exit 4
+        exit 4
+fi
+
+if [ ! -f "$LAUNCH" ]
+then
+  echo "$LAUNCH does not exists!"
+        exit 5
 fi
 
 if [ ! -d "$CATKIN_WS" ]
 then
   echo "$CATKIN_WS does not exists!"
-	exit 5
+        exit 6
 fi
 
 # source ros stuff
@@ -110,7 +128,7 @@ fi
 . "$CATKIN_WS"/devel/setup.bash
 
 # avoid port number smaller than 11320 and higher than 15320
-PORT=$((11320 + $TRAIL%(15320-11320)))
+PORT=$((11320 + $TRIAL%(15320-11320)))
 
 # get bag duration
 BAG_DURATION=$(rosbag info "$BAGFILE" | grep -oP 'duration[\s\:s\d]*\(\K(\d*)')
@@ -121,7 +139,7 @@ do
   PORT=$(($PORT+1))
 done
 
-# set indivdual port for each trial (to run multiple trails in parallel)
+# set indivdual port for each trial (to run multiple trials in parallel)
 export ROS_MASTER_URI=http://localhost:$PORT
 
 # start roscore
@@ -129,11 +147,7 @@ timeout $(($BAG_DURATION+6))s nice -n -5 roscore -p $PORT &> "$LOGDIR"/out.log &
 sleep 2
 
 # start odometry
-timeout $(($BAG_DURATION+2))s roslaunch drive_ros_imu_odo_odometry imu_odo_odometry.launch  debug_file:=true debug_file_path:="$LOGDIR"/odom.csv vehicle_config:="$VEHICLECONFIG" &> "$LOGDIR"/out.log &
-sleep 1
-
-# play back the rosbag
-nice -n -5 rosbag play "$BAGFILE" -q &> "$LOGDIR"/out.log
+roslaunch drive_ros_imu_odo_odometry "$LAUNCH" debug_out:=true debug_out_file_path:="$LOGDIR"/odom.csv vehicle_config:="$VEHICLECONFIG" &> "$LOGDIR"/out.log &
 
 exit 0
 
